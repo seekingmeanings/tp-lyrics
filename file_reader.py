059b38a0795da4b3fa23224261656bb8685cf10f
TheEnd <uwilldiesoon@duck.com>
2022-04-08 17:34:53 +0200

    yes! Progress! - i now have a working template, it has to be implemented though
    - lyrics can be extracted from the "database"
    - genius stuffca be implemented
    
    'numbers':
    	- file to test the file_reader.py program

---
+++ file_reader.py
@@ -0,0 +1,34 @@
+#/usr/bin/env python3
+import sys
+from subprocess import run
+from math import ceil
+
+
+file_name="/data/data/com.termux/files/home/lyrics/numbers"
+lines_per_site=11
+notification_id=3933
+
+def create_not(content):
+    run(["termux-notification", "-i", str(notification_id),  "--button1", "previous", "--button1-action",\
+         "env python3 $HOME/lyrics/n_reader.py {}".format(idx-1), "--button2", "next", "--button2-action", \
+         "env python3 $HOME/lyrics/n_reader.py {}".format(idx+1), "--button3", "exit", "--button3-action", \
+         "termux-notification-remove {}".format(notification_id), "-t", file_name, "-c", ''.join(content) ])
+
+with open(file_name, 'r') as f:
+    nums=[line for line in f]
+
+idx=int(sys.argv[1])
+_ceiled_val=ceil(int(len(nums) / lines_per_site))
+if idx < 0:
+    raise IndexError("index is negative")
+elif idx > _ceiled_val:
+    raise IndexError("index out of range")
+elif idx == _ceiled_val:
+    #that means. that the list is smaller than the screen
+    #so the index for the print has to be set to max
+    create_not(nums[idx*lines_per_site:])
+else:
+    create_not(nums[idx*lines_per_site:(idx+1)*lines_per_site])
+
+
+    
--- get_lyrics.py
+++ get_lyrics.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
+import subprocess as sp
+from time import asctime
+from notification import getCurrent
 import sys
-import lyricsgenius as lg
 
-genius = lg.Genius('Client_Access_Token_Goes_Here', skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
 
-def get_lyrics(arr, k):
-    c = 0
-    for name in arr:
-        try:
-            songs = (genius.search_artist(name, max_songs=k, sort='popularity')).songs
-            s = [song.lyrics for song in songs]
-            file.write("\n \n   <|endoftext|>   \n \n".join(s))
-            c += 1
-            print(f"Songs grabbed:{len(s)}")
-        except:
-            print(f"some exception at {name}: {c}")
+def toast(msg, position="bottom", bc="white", tc="black"):
+    sp.run(['termux-toast', '-g', position, '-b', bc, '-c', tc, msg])
 
-if __name__=='__main__':
-    if len(sys.argv) < 2:
-        print('usage: getLyrics numberOfSongs Artist(s)')
-        sys.exit()
-    with open('test.lrc', 'w') as file:
-        get_lyrics(sys.argv[2:], sys.argv[1])
+#make sure the api is running
+sp.run("termux-api-start")
+
+
+def getLyrics(current):
+    if len(current) != 1: raise IndexError("failed to get the current song name.\nGot: {}".format(current))
+    try:
+        with open("data/{artist}%{song}".format(artist=current[0][0],song=current[0][1]), 'r') as f:
+            toast("lyrics found")
+            return f.readlines()
+    except FileNotFoundError:
+        toast("no lyrics found")
+        return False
+
+
+if __name__ == "__main__":
+    print(ck_lyrics(getCurrent()))

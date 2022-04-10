a4d28709b69d710dc8be0c771ebb40cd4b563bee
TheEnd <uwilldiesoon@duck.com>
2022-04-10 22:19:08 +0200

    made file modular, so it can be used via import and as a script

--- file_reader.py
+++ file_reader.py
@@ -7,45 +7,39 @@ from subprocess import run
 from math import ceil
 from random import randint
 
-LINES_PER_SITE=11
+def open_file(file_name:str,nid:int,idx:int):
+    LINES_PER_SITE=11
+    SELF_CALL=f"env python3 $HOME/lyrics/file_reader.py -i {nid} -f '{file_name}'"\
+        + " -p {i}"
 
+    with open(file_name, 'r') as f:
+        nums=[line for line in f]
+        
+        _ceiled_val=ceil(int(len(nums) / LINES_PER_SITE))
+        if idx < 0:
+            raise IndexError("index is negative")
+        elif idx > _ceiled_val:
+            raise IndexError("index out of range")
+        elif idx == _ceiled_val:
+            #that means. that the list is smaller than the screen
+            #so the index for the print has to be set to max
+            content=nums[idx*LINES_PER_SITE:]
+        else:
+            content=nums[idx*LINES_PER_SITE:(idx+1)*LINES_PER_SITE]
+            
+            
+            run(["termux-notification", "-i", str(nid),  "--button1", "previous",\
+                 "--button1-action", SELF_CALL.format(i=idx-1), "--button2", "next",\
+                 "--button2-action", SELF_CALL.format(i=idx+1), "--button3", "exit", \
+                 "--button3-action", f"termux-notification-remove {nid}",\
+                 "--ongoing", "-t", file_name, "-c", ''.join(content) ])
+            
+            
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--notification_id", type=int, default=randint(10,10000))
+    parser.add_argument("-f", "--file_name", type=str)
+    parser.add_argument("-p", "--page", type=int, default=0)
+    args=parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--notification_id", type=int, default=randint(10,10000))
-parser.add_argument("-f", "--file_name", type=str)
-parser.add_argument("-p", "--page", type=str, default=0)
-args=parser.parse_args()
-
-
-file_name=args.file_name
-nid=args.notification_id
-idx=int(args.page)
-
-
-SELF_CALL=f"env python3 $HOME/lyrics/file_reader.py -i {nid} -f '{file_name}'"\
-    + " -p {i}"
-
-    
-with open(file_name, 'r') as f:
-    nums=[line for line in f]
-
-
-_ceiled_val=ceil(int(len(nums) / LINES_PER_SITE))
-if idx < 0:
-    raise IndexError("index is negative")
-elif idx > _ceiled_val:
-    raise IndexError("index out of range")
-elif idx == _ceiled_val:
-    #that means. that the list is smaller than the screen
-    #so the index for the print has to be set to max
-    content=nums[idx*LINES_PER_SITE:]
-else:
-    content=nums[idx*LINES_PER_SITE:(idx+1)*LINES_PER_SITE]
-
- 
-run(["termux-notification", "-i", str(nid),  "--button1", "previous",\
-     "--button1-action", SELF_CALL.format(i=idx-1), "--button2", "next",\
-     "--button2-action", SELF_CALL.format(i=idx+1), "--button3", "exit", \
-     "--button3-action", f"termux-notification-remove {nid}",\
-     "--ongoing", "-t", file_name, "-c", ''.join(content) ])
-
+    open_file(file_name=args.file_name, nid=args.notification_id,idx=args.page)

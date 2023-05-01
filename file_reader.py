226bbbf490d89e6ac15e7f9f918708f924da0534
seekingmeanings <git-j@mail.o-family.de>
2023-05-01 22:59:33 +0200

    fixed display bug

--- file_reader.py
+++ file_reader.py
@@ -8,23 +8,25 @@ from random import randint
 
 from utils import termux_api_toast as toast
 
+
 HOME_DIR = "/data/data/com.termux/files/home"
+DEBUG = True
 
 
 def open_file(file_name: str, nid: int,
-              idx: int = 0, fp: bool = False):
-    # fp stands for full_path
-    
+              page_idx: int = 0, full_path: bool = False):
     LINES_PER_SITE = 11
-    SELF_CALL = "env python3 $HOME/lyrics/file_reader.py " +\
-        f"-i {nid} -f '{file_name}'"\
-        + (" -m" if fp else "") + " -p {i}"
+    SELF_CALL = "python3 $HOME/tp-lyrics/file_reader.py " +\
+        f"-i {nid} -f '{file_name}'" +\
+        (" -m" if full_path else "") + " -p {i}" +\
+        (" >$HOME/tpl.log 2>&1" if DEBUG else "")
 
-    print(SELF_CALL)
     with open(file_name, 'r') as f:
-        nums = [line for line in f]
+        file_content = [line for line in f]
 
-    if len(nums) == 0:
+    if len(file_content) == 0:
+        # the file is empty
+        
         # importing it here 'cause it's rarly used
         from re import sub as rs
         r_e = f"^{HOME_DIR}"
@@ -32,34 +34,34 @@ def open_file(file_name: str, nid: int,
         run(["termux-notification", "-i", str(nid), "--button1", "exit",
              "--button1-action", f"termux-notification-remove {nid}", "-t",
              f"file '{rs(r_e, '~', file_name)[:50]}' is empty"
-             if fp else "{} (empty cache)"
+             if full_path else "{} (empty cache)"
              .format(file_name.split('/')[-1].replace('%', ' - ')),
-             "-c", '' if fp else f"file '{rs(r_e, '~', file_name)}' is empty"],
+             "-c", '' if full_path
+             else f"file '{rs(r_e, '~', file_name)}' is empty"],
             check=True)
     else:
-        _ceiled_val = ceil(int(len(nums) / LINES_PER_SITE))
-        if idx < 0 or idx > _ceiled_val:
+        pages_in_file = ceil(int(len(file_content) / LINES_PER_SITE))
+        if page_idx < 0 or page_idx > pages_in_file:
             raise IndexError("index OOR")
 
-        if idx == _ceiled_val:
+        if page_idx == pages_in_file:
             # that means. that the list is smaller than the screen
             # so the index for the print has to be set to max
-            content = nums[idx*LINES_PER_SITE:]
+            content = file_content[page_idx*LINES_PER_SITE:]
         else:
-            content = nums[idx*LINES_PER_SITE:(idx+1)*LINES_PER_SITE]
+            content = file_content[page_idx*LINES_PER_SITE:(page_idx+1)*LINES_PER_SITE]
+            toast("remainder")
 
-        toast(content)
-        
-        # really inside of the if else statement?
+        print(SELF_CALL.format(i=page_idx))
         run(["termux-notification", "-i", str(nid),
              "--button1", "previous",
-             "--button1-action", SELF_CALL.format(i=idx-1),
+             "--button1-action", SELF_CALL.format(i=page_idx-1),
              "--button2", "next",
-             "--button2-action", SELF_CALL.format(i=idx+1),
+             "--button2-action", SELF_CALL.format(i=page_idx+1),
              "--button3", "exit",
              "--button3-action", f"termux-notification-remove {nid}",
              "--alert-once",
-             "-t", file_name if fp else
+             "-t", file_name if full_path else
              file_name.split('/')[-1].replace('%', ' - '),
              "-c", ''.join(content)],
             check=True)
@@ -74,4 +76,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     open_file(file_name=args.file_name, nid=int(args.notification_id),
-              idx=int(args.page), fp=args.music_mode)
+              page_idx=int(args.page), full_path=args.music_mode)
